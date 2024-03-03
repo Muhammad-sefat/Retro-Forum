e4c5b1f01653afa3c3cabfbc6148fbb2d73d548a
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-03-03 16:31:49 +0600

    finish showing latest section card in javscript

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ const getAllFetchData = async () =>{
     const cardContainer = document.getElementById('card-container');
 
     allData.forEach((item)=>{
-        console.log(item);
+        // console.log(item);
         const newDiv = document.createElement('div');
-        newDiv.className = "mb-10"
         newDiv.innerHTML = `
         <div class="flex flex-col md:flex-row items-center gap-5 bg-[#F3F3F5] rounded-xl p-5 mb-10">
                         <div class="relative md:w-[20%]">
@@ -47,4 +46,38 @@ const getAllFetchData = async () =>{
         cardContainer.appendChild(newDiv);
     })
 }
+
+const getAllLatestData = async ()=>{
+    const response = await fetch(" https://openapi.programming-hero.com/api/retro-forum/latest-posts");
+    const data = await response.json();
+    
+    const latestCardContainer = document.getElementById("latest-card-container");
+
+    data.forEach((item)=>{
+        console.log(item);
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <div class="card bg-base-100 shadow-xl">
+                        <figure><img src="${item.cover_image}" /></figure>
+                        <div class="card-body">
+                          <div class="flex items-center gap-3">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <p>${item.author.posted_date}</p>
+                          </div>
+                          <h2 class="text-lg font-extrabold">${item.title}</h2>
+                          <p class="text-[#666] py-1 font-medium">${item.description}</p>
+                          <div class="flex gap-3">
+                            <img class ="w-[30%] rounded-full" src="${item.profile_image}">
+                            <div>
+                                <h2 class="font-bold text-lg">${item.author.name}</h2>
+                                <p class="text-[#666]">${item.author.designation}</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+        `;
+        latestCardContainer.appendChild(newDiv);
+    })
+}
+getAllLatestData();
 getAllFetchData();
