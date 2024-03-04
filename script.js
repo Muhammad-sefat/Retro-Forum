ef6694a15b6ce0b58c721f3b54bb4e0adc917d0e
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-03-04 07:40:51 +0600

    complete searchbyId and showing

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const getAllFetchData = async () =>{
     const allData = data.posts;
 
     const cardContainer = document.getElementById('card-container');
-
     allData.forEach((item)=>{
         const newDiv = document.createElement('div');
         newDiv.innerHTML = `
@@ -112,6 +111,61 @@ function getData(title,view){
 
 }
 
+const searchBtn = async ()=>{
+    loadingBar(true);
+    const inputBtn = document.getElementById('input-btn')
+    const inputValueById = inputBtn.value;
+    getCategoryById(inputValueById); 
+}
+const getCategoryById = async (inputValueById)=>{
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${inputValueById}`);
+    const data = await response.json();
+    const allData = data.posts;
+
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = "";
+    allData.forEach((item)=>{
+        const newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+        <div class="flex flex-col md:flex-row items-center gap-5 bg-[#F3F3F5] rounded-xl p-5 mb-10">
+                        <div class="relative md:w-[20%]">
+                            <i class="fa-solid fa-circle right-0 -top-2  text-base absolute ${item.isActive?"text-green-700":"text-red-700"}"></i>
+                            <img class="rounded-xl" src="${item.image}">
+                        </div>
+                        <div class = "md:w-[70%]">
+                            <div class="flex items-center gap-8">
+                                <p class="text-base font-medium">#${item.category}</p>
+                                <p class="text-base font-medium">Author : ${item.author.name}</p>
+                            </div>
+                            <h2 id="title" class="text-2xl font-bold py-5">${item.title}</h2>
+                            <p class="text-base text-[#666] leading-normal pb-3">${item. description}</p>
+                            <hr class="border-dotted border border-gray-400 mb-4">
+                            <div class="flex items-center justify-between gap-5">
+                                <div class="flex">
+                                    <div class="flex items-center pr-5">
+                                        <i id="fa-regular" class="fa-regular fa-message text-base text-[#666] mr-2 font-medium"></i>
+                                        <p class="text-base text-[#666] font-medium">${item.comment_count}</p>
+                                    </div>
+                                    <div class="flex items-center pr-5">
+                                        <i class="fa-regular fa-eye text-base text-[#666] mr-2 font-medium"></i>
+                                        <p  class="text-base text-[#666] font-medium">${item.view_count}</p>
+                                    </div>
+                                    <div class="flex items-center pr-5">
+                                        <i class="fa-regular fa-clock text-base text-[#666] mr-2 font-medium"></i>
+                                        <p  class="text-base text-[#666] font-medium">${item.posted_time} min</p>
+                                    </div>
+                                </div>
+                                <p onclick="getData('${item.title.replace("'","")}',${item.view_count})"><i class="fa-regular fa-envelope bg-lime-600 text-white p-2 rounded-full"></i></p>
+                            </div>
+                        </div>
+
+                    </div>
+        `;
+        cardContainer.appendChild(newDiv);
+    })
+    loadingBar(false)
+}
+
 const loadingBar = (isLoding)=>{
     const loadingbar = document.getElementById('loading-bar');
    if(isLoding){
@@ -127,18 +181,5 @@ function addNumberElementById(elementId,value){
     element.innerText = value;
 }
 
-function searchBtn (){
-    const inputBtn = document.getElementById('input-btn')
-    const inputValue = inputBtn.value;
-    return inputValue;
-}
-const getCategoryById = async ()=>{
-    const response = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts?category=coding");
-    const data = await response.json();
-    console.log(data);
-}
-
-
-getCategoryById();
 getAllLatestData();
 getAllFetchData();
